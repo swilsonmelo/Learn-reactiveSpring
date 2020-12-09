436584112b5f9c90793e479b8675b1d47aba1318
willson.melo <swilsonmelo@gmail.com>
2020-12-09 00:01:00 -0500

    finished transforming a reactive stream using flatMap

--- src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTransformTest.java
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTransformTest.java
@@ -7,6 +7,8 @@ import reactor.test.StepVerifier;
 import java.util.Arrays;
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FluxAndMonoTransformTest {
 
     List<String> names = Arrays.asList("adam", "anna", "jack", "jenny");
@@ -61,6 +63,72 @@ public class FluxAndMonoTransformTest {
 
         StepVerifier.create(namesFlux)
                 .expectNext("JENNY")
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void transformUsingFlatMap(){
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A","B","C","D","E","F"))
+                .flatMap(s -> {
+
+                    return Flux.fromIterable(convertToList(s));// A -> List[A, newValue]
+                })
+                .log(); //Db or external calls for each element that returns a Flux: s -> Flux<String>
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    private List<String> convertToList(String s) {
+
+        try {
+            Thread.sleep(1000); // we are simulating a db call
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(s, "new value");
+
+    }
+
+    @Test
+    public void transformUsingParallel(){
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A","B","C","D","E","F")) // Flux<String>
+                .window(2) // Flux<Flux<String>> (A,B), (C,D) it are going to wait for two items
+                .flatMap(s ->
+                    s.map(this::convertToList).subscribeOn(parallel()) // Flux<List<String>>
+                            .flatMap(Flux::fromIterable) // Flux<String>
+                )
+                .log(); //Db or external calls for each element that returns a Flux: s -> Flux<String>
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    // Holy ****, mind blow...
+    @Test
+    public void transformUsing_Parallel_Maintain_Order(){
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A","B","C","D","E","F")) // Flux<String>
+                .window(2) // Flux<Flux<String>> (A,B), (C,D) it are going to wait for two items
+                /*.concatMap(s -> // Slower
+                        s.map(this::convertToList).subscribeOn(parallel()) // Flux<List<String>>
+                                .flatMap(Flux::fromIterable) // Flux<String>
+                )*/
+                .flatMapSequential(s -> // Faster
+                        s.map(this::convertToList).subscribeOn(parallel()) // Flux<List<String>>
+                                .flatMap(Flux::fromIterable) // Flux<String>
+                )
+                .log(); //Db or external calls for each element that returns a Flux: s -> Flux<String>
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
                 .verifyComplete();
 
     }
