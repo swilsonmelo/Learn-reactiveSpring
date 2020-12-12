5d920530bb8d3d0abafddc650b7070a2bbc8f8a8
willson.melo <swilsonmelo@gmail.com>
2020-12-12 18:37:59 -0500

    Finished Virtualizaing Time in JUnit

--- src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -3,6 +3,7 @@ package com.learnreactivespring.fluxandmonoplayground;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
 
@@ -60,16 +61,26 @@ public class FluxAndMonoCombineTest {
     @Test
     public void combineUsingConcat_WithDelay() {
 
+        VirtualTimeScheduler.getOrSet();
+
         Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
         Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
 
         Flux<String> mergedFlux = Flux.concat(flux1, flux2)
                 .log();
 
-        StepVerifier.create(mergedFlux)
+        // with virtual time the duration of the test is 1 second ...
+        StepVerifier.withVirtualTime(() -> mergedFlux)
                 .expectSubscription()
+                .thenAwait(Duration.ofSeconds(6))
                 .expectNext("A", "B", "C", "D", "E", "F")
                 .verifyComplete();
+
+        // the duration of the test is 6 seconds...
+        /*StepVerifier.create(mergedFlux)
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();*/
 
     }
 
